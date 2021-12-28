9e733c2530ddf7bbba5709615806e17af77bc5bf
doubleyou77 <21sunrin077@sunrint.hs.kr>
2021-12-28 12:40:29 +0900

    2021-12-28 3

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+//const mongoose = require('mongoose');
 
+const app = express();
+
+//mongoose.connect('mongodb://dbdb:dbdb@localhost:27017/mydb');
+
+app.use(express.json());
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -9,13 +13,22 @@ app.use(express.static(__dirname + '/public'));
 const Director = ['조재호', '안용호', '오연호', '심영수', '이혜찬', '민경록', '김형진'];
 
 app.get('/', (req, res) => {
-    res.render('index', {Director});
+    res.render('index', { Director });
 });
 
 app.post('/', async (req, res) => {
     const { contents } = req.body;
 
     await checkDirector(contents);
+    res.send('success');
+});
+
+app.get('/addDirector', (req, res) => {
+    res.render('addDirector');
+});
+
+app.post('/addDirector', (req, res) => {
+    res.render('index', { Director });
 });
 
 app.get('*', (req, res) => {
@@ -27,13 +40,7 @@ app.listen(8080, () => {
 });
 
 function checkDirector(contents) {
-    switch(contents) {
-        case '조재호' : console.log('조재호'); break;
-        case '안용호' : console.log('안용호'); break;
-        case '오연호' : console.log('오현호'); break;
-        case '심영수' : console.log('오연호'); break;
-        case '이혜찬' : console.log('이혜찬'); break;
-        case '민경록' : console.log('민경록'); break;
-        case '김형진' : console.log('김형진'); break;
-    }
+    if(Director.find(e=>e===contents)) {
+        console.log(contents);
+    };
 }
