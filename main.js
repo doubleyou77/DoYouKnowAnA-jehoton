8b1506eb6c53c683c89c4f850582c4f20ad31491
double <21sunrin077@sunrint.hs.kr>
2021-12-29 00:27:45 +0900

    2021-12-29

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const res = require('express/lib/response');
 const mongoose = require('mongoose');
 const directorModel = require('./models/Director');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/jehoton',
+mongoose.connect('mongodb://localhost:27017/jehoton',
 {
     useNewUrlParser: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(e => {
+    console.log(e);
 });
 
 app.use(express.json());
@@ -14,9 +19,18 @@ app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-const Director = ['조재호', '안용호', '오연호', '심영수', '이혜찬', '민경록', '김형진'];
+let Director = [];
+let Directorprofile = [];
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    try {
+        const directorInstance = await directorModel.find({}, {"_id":0, "directorName":1});
+        console.log('부장 목록 : ' + directorInstance);
+        Director = directorInstance.map(i => i.directorName).slice()
+        console.log(Director, typeof Director);
+    } catch(err) {
+        console.log('에러');
+    }
     res.render('index', { Director });
 });
 
@@ -24,22 +38,55 @@ app.post('/', async (req, res) => {
     const { contents } = req.body;
 
     await checkDirector(contents);
-    res.send('success');
+    console.log(Directorprofile);
+    res.render('index', { Director, Directorprofile });
+    //데이터 프론트로 넘기기
 });
 
 app.get('/addDirector', (req, res) => {
     res.render('addDirector');
 });
 
+//부장 추가
 app.post('/addDirector', async (req, res) => {
     const { directorName, directorInf } = req.body;
-
-    await directorModel.create({ 
-        directorName, directorInf
-    });
     
+    try { 
+        await directorModel.create({
+            directorName, directorInf
+        });
+        Director.push(directorName);
+        return res.status(200).json(directorModel);
+    } catch(err) {
+        return res.status(500).json({ error: err });
+    }
 });
 
+//부장 목록 검색
+app.get('/selectDirector', async (req, res) => {
+    try {
+        const directorInstance = await directorModel.find();
+        console.log('부장 목록 : ' + directorInstance);
+        return res.status(200).json(directorInstance);
+    } catch(err) {
+        console.log('에러');
+        return res.status(500).json({error:err});
+    }
+});
+
+//주의 데이터삭제-----------------------------------------------------------
+app.get('/deleteDirector', async (req, res) => {
+    try {
+        const deleteData = await directorModel.deleteMany();
+        console.log('초기화');
+        res.send(`<script>window.location.href = "/"</script>`);
+    } catch(err) {
+        console.log('에러');
+        return res.status(500).json({error:err});
+    }
+})
+
+//페이지 접근제한
 app.get('*', (req, res) => {
     res.send(`<script>history.back()</script>`);
 });
@@ -48,8 +95,11 @@ app.listen(8080, () => {
     console.log('server start');
 });
 
-function checkDirector(contents) {
+async function checkDirector(contents) {
     if(Director.find(e=>e===contents)) {
-        console.log(contents);
+        console.log(contents)
+        const directorInstance = await directorModel.find({directorName: contents}, {"_id":0, "directorInf":1});
+        Directorprofile = directorInstance.map(i => i.directorInf).slice();
+        console.log(contents, Directorprofile);
     };
 }
